7bc93a26b488dd9988a95cbcc0f4cd20c5d8851c
kah230895 <kaiqueoliveiraantonio@gmail.com>
2024-05-03 14:58:11 -0300

    test

--- packages/docs/src/stories/Avatar.stories.tsx
+++ packages/docs/src/stories/Avatar.stories.tsx
@@ -1,30 +1,30 @@
-import { Meta, StoryObj } from '@storybook/react'
-import { Avatar, AvatarProps } from '@kaique-ignite-ui/react'
+import { Meta, StoryObj } from "@storybook/react";
+import { Avatar, AvatarProps } from "@kaique-ignite-ui/react";
 
 export default {
-  title: 'Data display/Avatar',
+  title: "Data display/Avatar",
   component: Avatar,
 
   args: {
-    src: 'https://github.com/kaique-oliveira.png',
-    alt: 'Kaique oliveira',
+    src: "https://github.com/kaique-oliveira.png",
+    alt: "Kaique oliveira Antonio",
   },
 
   argTypes: {
     src: {
-      description: 'Avatar para ser utilizada foto do usuário.',
+      description: "Avatar para ser utilizada foto do usuário.",
       control: {
-        type: 'text',
+        type: "text",
       },
     },
   },
-} as Meta<AvatarProps>
+} as Meta<AvatarProps>;
 
-export const Primary: StoryObj<AvatarProps> = {}
+export const Primary: StoryObj<AvatarProps> = {};
 
 export const WithFallBack: StoryObj<AvatarProps> = {
   args: {
-    src: '',
-    alt: 'Kaique oliveira',
+    src: "",
+    alt: "Kaique oliveira",
   },
-}
+};
