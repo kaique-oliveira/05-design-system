23593e90c6156a00c233adab10daf18cccdce5a3
kah230895 <kaiqueoliveiraantonio@gmail.com>
2024-05-03 10:28:38 -0300

    edit

--- packages/react/src/components/MultiStep/styles.ts
+++ packages/react/src/components/MultiStep/styles.ts
@@ -1,33 +1,33 @@
-import { styled } from '../../styles'
-import { Text } from '../Text'
+import { styled } from "../../styles";
+import { Text } from "../Text";
 
-export const MultiStepContainer = styled('div', {})
+export const MultiStepContainer = styled("div", {});
 
 export const Label = styled(Text, {
-  color: '$gray200',
+  color: "$gray200",
 
   defaultVariants: {
-    size: 'xs',
+    size: "xs",
   },
-})
+});
 
-export const Steps = styled('div', {
-  display: 'grid',
-  gap: '$2',
-  marginTop: '$1',
-  gridTemplateColumns: 'repeat(var(--steps-size), 1fr)',
-})
+export const Steps = styled("div", {
+  display: "grid",
+  gap: "$2",
+  marginTop: "$1",
+  gridTemplateColumns: "repeat(var(--steps-size), 1fr)",
+});
 
-export const Step = styled('div', {
-  height: '$1',
-  borderRadius: '$px',
-  background: '$gray600',
+export const Step = styled("div", {
+  height: "$1",
+  borderRadius: "$px",
+  background: "$gray600",
 
   variants: {
     active: {
       true: {
-        background: '$gray100',
+        background: "$gray100",
       },
     },
   },
-})
+});
